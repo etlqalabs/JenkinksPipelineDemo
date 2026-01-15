c052b5e4f3271cd2be47e9d13fa040788d151e6c
Hetu <sahu.hetram@gmail.com>
2026-01-15 17:47:57 +0530

    jenkins

--- Test Scripts/test_regression_test_cases.py
+++ Test Scripts/test_regression_test_cases.py
@@ -7,8 +7,8 @@ class TestRegression:
 
     @pytest.mark.regression
     def test_regression_test2(self):
-        assert 1 == 2 ,"Both numbers are not equal"
+        assert 2 == 2 ,"Both numbers are not equal"
 
     @pytest.mark.regression
     def test_regression_test3(self):
-        assert 1 == 3 ,"Both numbers are not equal"
+        assert 3 == 3 ,"Both numbers are not equal"
